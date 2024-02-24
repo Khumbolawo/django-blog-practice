77c02b18ad6be44bcdeeb168192d22a6454f3f82
Khumbolawo <khumbo87@gmail.com>
2024-02-24 22:55:49 +0200

    Added front and backend for creating and deleting

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,49 @@
+from django.forms import BaseModelForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import (ListView, 
+                                  DetailView, 
+                                  CreateView,
+                                  UpdateView)
 
 # Create your views here.
 
+#function based views for home page i learned first
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
+#class based view for home page
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+#function based view for about page
 def about(request):
     return render(request, 'blog/about.html', {'title': 'about'})
 
